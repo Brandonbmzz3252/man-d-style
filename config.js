84543bffa2e62961429f2adc120b1eb5c60d439a
Brandonbmzz3252 <brandonbmzz3252@users.noreply.github.com>
2026-09-12 08:59:13 +0200

    Connect booking sheet

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // MAN-D-STYLE shared availability backend
 // Paste your deployed Apps Script /exec URL between the quotes.
 // Leave empty ("") to run in local-only mode.
-window.MDS_BACKEND_URL = "";
+window.MDS_BACKEND_URL = "https://script.google.com/macros/s/AKfycbxNhpKkqKGhfJi2q5gTDDty6WiTwZivmi4o7VcyUtukG2ze8asj4WikP-VRdWICbx-v-w/exec";
